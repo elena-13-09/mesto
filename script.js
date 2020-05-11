4ac5a6c714476e75d2185a0913a2a46927078c30
Elena Goreva <elena13.09.90@yandex.ru>
2020-05-11 12:32:07 +0300

    исправление первого ревью

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ const popup = document.querySelector('.popup');
 const popupButton = document.querySelector('.profile__edit-button');
 const popupClouse = document.querySelector('.popup__close');
 const popupSubmit = document.querySelector('.popup__submit');
-const profileName = document.querySelector('.profile__name');
-const profileProfession = document.querySelector('.profile__profession');
-const inputName = document.querySelector('.popup__name');
-const inputProfession = document.querySelector('.popup__profession');
+
+let profileName = document.querySelector('.profile__name');
+let profileProfession = document.querySelector('.profile__profession');
+let inputName = document.querySelector('.popup__input_name');
+let inputProfession = document.querySelector('.popup__input_profession');
 
 function formOpened() {
   popup.classList.toggle('popup_opened');
